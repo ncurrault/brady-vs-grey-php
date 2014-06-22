649a91f965f2dcb7bc1c7c5b0b7f0e7c8c910312
nicholas818 <cyberchase818@aim.com>
2014-06-22 16:10:32 -0700

    Added comments; set up some useful functions.
    I’m about to begin the actual thingy.

--- update.php
+++ update.php
@@ -1,7 +1,6 @@
 <?php
 
-// http_response_code(501); // NOT IMPLEMENTED
-
+// Some handy SQL functions
 function sql_ret($q)
 {
 	$sql_connection = mysqli_connect($host="localhost", "root", "root", "my_db_2", 8889);
@@ -29,7 +28,6 @@ function sql_ret($q)
 		return $ret;
 	}
 }
-
 function sql_no_ret($q)
 {
 	$sql_connection = mysqli_connect($host="localhost", "root", "root", "my_db_2", 8889);
@@ -47,18 +45,19 @@ function sql_no_ret($q)
 			echo "Error with query";
 		}
 	}
+	
+	/* I'll use these later
+	 * "DELETE FROM $table WHERE PID=$pid"
+	 * "INSERT INTO Video (Title, YouTubeID, UploadDate, Channel, Creator, ViewCount) VALUES ('Title', 'ABC123ID456', '2014-06-17 23:23:00', 'deastruction', 'Dylan Hong', 301)"
+	 */
 }
 
-function sql_remove($table, $pid)
-{
-	sql_no_ret("DELETE FROM $table WHERE PID=$pid");
-}
-// sql_no_ret("INSERT INTO Video (Title, YouTubeID, UploadDate, Channel, Creator, ViewCount) VALUES ('Title', 'ABC123ID456', '2014-06-17 23:23:00', 'deastruction', 'Dylan Hong', 301)");
-
+// Get the API key from my file
 $key_file = fopen('api_key.txt', 'r');
-$api_key = fread($file, 39);
+$api_key = fread($key_file, 39);
 fclose($key_file);
 
+// The API needs its library in the include_path.
 set_include_path(
 	get_include_path()
 	.
@@ -67,11 +66,46 @@ set_include_path(
 	$_SERVER['DOCUMENT_ROOT'] . "/ZendFramework-1.12.7/library"
 );
 
-
+// Initialize the API with my key
 require_once 'Zend/Loader.php'; // the Zend dir must be in your include_path
 Zend_Loader::loadClass('Zend_Gdata_YouTube');
-$yt = new Zend_Gdata_YouTube();
+$yt = new Zend_Gdata_YouTube(null, "Brady vs. Grey - PHP version", null, $api_key);
+$yt->setMajorProtocolVersion(2);
 
+// Functions for getting videos from the API and putting them in arrays
+function vidEntryToArray($videoEntry, $channel) 
+{
+	$ret = array(
+	'Title' => $videoEntry->getVideoTitle(),
+	'YouTubeID' => $videoEntry->getVideoId(),
+	'ViewCount' => $videoEntry->getVideoViewCount(),
+	'Channel' => $channel
+	'UploadDate' => $videoEntry->mediaGroup->uploaded->text,
+	);
+	
+	if (GREY_CHANNELS[$channel])
+	{
+		$ret["Creator"] = "C.G.P. Grey";
+	}
+	else
+	{
+		$ret["Creator"] = "Brady Haran";
+	} 
+	
+	return $ret;
+}
+function getUploads(channel)
+{
+	$ret = array( );
+	$videoFeed = $yt->getUserUploads($channel);
+	
+	foreach($videoFeed as $vid)
+	{
+		$ret.push(vidEntryToArray($vid));
+	}
+	
+	return $ret;
+}
 
-
+echo "Done!";
 ?>
