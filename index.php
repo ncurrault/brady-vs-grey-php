8345d6d0acc6f186123498e3826a97e88baa761f
nicktendo64 <nicholas.curr+dev@gmail.com>
2014-06-24 09:43:33 -0700

    HTML modifications.
    1. I changed "uploaded" to "published" (to more accurately reflect the API output.
    2. I added Google Analytics.

--- index.php
+++ index.php
@@ -4,6 +4,16 @@
 <head>
 	<title>Brady vs. Grey</title>
 	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-50387911-2', 'brady-vs-grey.herokuapp.com');
+  ga('send', 'pageview');
+
+</script>
+	<script>
 		function revealThings ()
 		{
 			for (var i in document.getElementsByClassName('hidden-to-grey'))
@@ -40,7 +50,7 @@ A:
 	<thead>
 		<th>Creator</th>
 		<th>Channel</th>
-		<th>Uploaded</th>
+		<th>Published</th>
 		<th>View Count</th>
 		<th>Title/Link</th>
 	</thead>
