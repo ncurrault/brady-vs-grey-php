d9297c748525c9ea2c3dbeb01a1aa3a1af3e9b68
nicktendo64 <nicholas.curr+dev@gmail.com>
2014-06-25 12:08:26 -0700

    Prohibited zooming (with viewport)

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 	<!-- Bootstrap -->
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 	
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
